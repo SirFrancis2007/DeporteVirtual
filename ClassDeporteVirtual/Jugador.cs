071d6a8648a5be0b467c89e95ad823d5c09a5d2b
SirFrancis2007 <132942551+SirFrancis2007@users.noreply.github.com>
2025-03-25 10:22:57 -0300

    Commit Add class Equipo

--- ClassDeporteVirtual/Jugador.cs
+++ ClassDeporteVirtual/Jugador.cs
@@ -1,7 +1,16 @@
-﻿namespace ClassDeporteVirtual
+﻿namespace DeporteVirtual
 {
-    public class Jugador
+    public abstract class Jugador
     {
+        public double visionJuego  { get; set; }
+        public double VisionGrupal { get; set; }
+        public double Potencia { get; set; }
+        public double HabilidadPases { get; set; }
+
+        public abstract double GetPresicion();
+        public abstract double GetVisionGeneral();
+        public abstract double AplicarEntrenamientoFisico();
+
 
     }
-}a
+}
